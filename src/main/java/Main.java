6aea6654a3945a9bba51905d6dc5f41dfc1729d3
Leonid Poluiakhtov <len4ik-0123@yandex.ru>
2024-07-30 16:03:22 +0400

    Практика ООП. Инкапсуляция #3. Студент

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        Fraction f1 = new Fraction(1, 3);
-        Fraction f2 = new Fraction(2, 5);
-        Fraction f3 = new Fraction(7, 8);
+        Student s1 = new Student("Витя"); //создание студента без набора оценок
+        System.out.println(s1);
+        s1.setGrades(4);
+        s1.setGrades(3);
+        s1.setGrades(5);
+        System.out.println(s1);
 
-        System.out.println(f1.sum(f2).sum(f3).minus(5));
+        List<Integer> gradesS2 = new ArrayList<>(); //создание списка оценок и создание студента с набором оценок
+        gradesS2.add(3);
+        gradesS2.add(5);
+        gradesS2.add(4);
+        gradesS2.add(2);
+        Student s2 = new Student("Оля", gradesS2);
+        System.out.println(s2);
+        s2.getGrades().clear();
+        System.out.println(s2);
+        s2.setGrades(2);
     }
 }
