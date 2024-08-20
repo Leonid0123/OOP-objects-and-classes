aaed1e162d3ad40578e358a6f36d2bb060043194
Leonid Poluiakhtov <len4ik-0123@yandex.ru>
2024-08-20 16:48:42 +0400

    Практика ООП. Полиморфизм #1. Трехмерная точка

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,36 +1,6 @@
-import java.util.ArrayList;
-import java.util.List;
-
 public class Main {
     public static void main(String[] args) {
-        List<Point> points = new ArrayList<>(); //1. Создать Ломаную, проходящую через точки {1;5}, {2;8}, {5;3}, {8,9}
-        points.add(new Point(1, 5));
-        points.add(new Point(2, 8));
-        points.add(new Point(5, 3));
-        points.add(new Point(8, 9));
-        PolyLine polyLine = new PolyLine(points);
-
-        double brokenLineLength = polyLine.getLength(); //2. Рассчитать длину Ломаной
-        System.out.println("Длина ломаной: " + brokenLineLength);
-        List<Line> arrayOfLines = polyLine.getLines(); //3. Получить у Ломаной массив Линий
-        System.out.println("Массив линий: " + arrayOfLines);
-
-        double arrayOfLinesLength=0; //4. Рассчитать длину массива Линий
-        for (Line line : arrayOfLines) {
-            arrayOfLinesLength = arrayOfLinesLength + line.getLength();
-        }
-        System.out.println("Длина массива линий: " + arrayOfLinesLength);
-
-        System.out.println("Длина ломаной и массива линий равны? " + (brokenLineLength == arrayOfLinesLength)); //5. Сравнить длину Ломаной и массива Линий: они должны совпасть
-        System.out.println("Изменяемая точка до изменений: " + points.get(1)); //6. Изменить координаты Точки {2,8} таким образом, чтобы она стала иметь значение {12,8}.
-        System.out.println("Изменяемая ломаная до изменений: " + polyLine);
-        System.out.println("Изменяемый массив до изменений: " + arrayOfLines);
-        if (points.get(1).getX() == 2 && points.get(1).getY() == 8) {
-            points.get(1).setX(12);
-            points.get(1).setY(8);
-        }
-        System.out.println("Изменяемая точка после изменений: " + points.get(1)); //Если изменения отразились в данной точке,
-        System.out.println("Изменяемая ломаная после изменений: " + polyLine); //в Ломаной
-        System.out.println("Изменяемый массив после изменений: " + arrayOfLines); //и в двух Линиях массива (из пункта 3), то задача решена верно.
+        Point3D point = new Point3D(1,2,3);
+        System.out.println(point);
     }
 }
---
+++ src/main/java/Point3D.java
@@ -0,0 +1,21 @@
+public class Point3D extends Point {
+    private int z;
+
+    public Point3D(int x, int y, int z) {
+        super(x, y);
+        this.z = z;
+    }
+
+    public int getZ() {
+        return z;
+    }
+
+    public void setZ(int z) {
+        this.z = z;
+    }
+
+    @Override
+    public String toString() {
+        return "{" + this.getX() + ";" + this.getY() + ";" + z + "}";
+    }
+}
