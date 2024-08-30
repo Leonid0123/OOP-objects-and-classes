3837db2fb3ce9d6172fec92d2908b35731bccbf2
lpoluyahtov <len4ik-0123@yandex.ru>
2024-08-30 22:09:01 +0300

    Практика ООП. Класс Object #2. Сравнение точек

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,12 @@
 public class Main {
-    public static void main(String[] args) {
-        Point3D point = new Point3D(1,2,3);
-        System.out.println(point);
+    public static void main(String[] args) throws CloneNotSupportedException {
+        Point p1 = new Point(1,2);
+        Point p2 = new Point(2, 3);
+        Point p3 = new Point(1,2);
+        System.out.println(p1.equals(p3)); //объекты равны
+        System.out.println(p1.equals(p2)); //объекты НЕ равны
+
+        Point p4 = p2.clone();
+        System.out.println(p2.equals(p4)); //объекты равны
     }
 }
