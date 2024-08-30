d3c22f71634e438c4ff0258575d2f79f52dd0461
lpoluyahtov <len4ik-0123@yandex.ru>
2024-08-31 00:44:36 +0300

    Практика ООП. Класс Object #4. Сравнение ломаных линий (2 вариант решения, упрощенный)

--- src/main/java/PolyLine.java
+++ src/main/java/PolyLine.java
@@ -40,13 +40,7 @@ public class PolyLine {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         PolyLine polyLine = (PolyLine) o;
-        List<Point> pointsThis = this.getPoints();
-        List<Point> pointsPolyLine = polyLine.getPoints();
-        if (pointsThis.size() != pointsPolyLine.size()) return false;
-        for (int i = 0; i < pointsThis.size(); i++) {
-            if (!pointsThis.get(i).equals(pointsPolyLine.get(i))) return false;
-        }
-        return true;
+        return points.equals(polyLine.points);
     }
 
     @Override
