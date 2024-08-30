981dab1bcc7c372cda62e0171a9fa7c5e965b261
lpoluyahtov <len4ik-0123@yandex.ru>
2024-08-31 00:19:52 +0300

    Практика ООП. Класс Object #4. Сравнение ломаных линий

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,17 +3,31 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        List<Point> points = new ArrayList<>(); //1. Создать Ломаную, проходящую через точки {1;5}, {2;8}, {5;3}, {8,9}
-        points.add(new Point(1, 5));
-        points.add(new Point(2, 8));
-        points.add(new Point(5, 3));
-        points.add(new Point(8, 9));
-        PolyLine polyLine = new PolyLine(points);
-        ClosedPolyLine closedPolyLine = new ClosedPolyLine(points);
+        //первая ломаная
+        List<Point> points1 = new ArrayList<>();
+        points1.add(new Point(1, 5));
+        points1.add(new Point(2, 8));
+        points1.add(new Point(5, 3));
+        points1.add(new Point(8, 9));
+        PolyLine polyLine1 = new PolyLine(points1);
 
-        double res1 = polyLine.getLength();
-        double res2 = closedPolyLine.getLength();
-        System.out.println("Длина ломаной линии: " + res1);
-        System.out.println("Длина замкнутой ломаной линии: " + res2);
+        //вторая ломаная
+        List<Point> points2 = new ArrayList<>();
+        points2.add(new Point(2, 3));
+        points2.add(new Point(4, 5));
+        points2.add(new Point(6, 7));
+        points2.add(new Point(8, 9));
+        PolyLine polyLine2 = new PolyLine(points2);
+
+        //третья ломаная (копия первой)
+        List<Point> points3 = new ArrayList<>(); //1. Создать Ломаную, проходящую через точки {1;5}, {2;8}, {5;3}, {8,9}
+        points3.add(new Point(1, 5));
+        points3.add(new Point(2, 8));
+        points3.add(new Point(5, 3));
+        points3.add(new Point(8, 9));
+        PolyLine polyLine3 = new PolyLine(points3);
+
+        System.out.println(polyLine1.equals(polyLine3)); //объекты равны
+        System.out.println(polyLine1.equals(polyLine2)); //объекты НЕ равны
     }
 }
--- src/main/java/PolyLine.java
+++ src/main/java/PolyLine.java
@@ -1,26 +1,27 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class PolyLine {
-    final private List<Point> polyLine;
+    final private List<Point> points;
 
     public PolyLine(List<Point> pointList) {
-        this.polyLine = pointList;
+        this.points = pointList;
     }
 
-    public List<Point> getPolyLine() {
-        return polyLine;
+    public List<Point> getPoints() {
+        return points;
     }
 
     @Override
     public String toString() {
-        return "Линия " + polyLine;
+        return "Линия " + points;
     }
 
     public List<Line> getLines() {
         List<Line> res = new ArrayList<>();
-        for (int i = 0; i < this.polyLine.size()-1; i++) {
-            res.add(new Line(polyLine.get(i),polyLine.get(i+1)));
+        for (int i = 0; i < this.points.size() - 1; i++) {
+            res.add(new Line(points.get(i), points.get(i + 1)));
         }
         return res;
     }
@@ -32,5 +33,24 @@ public class PolyLine {
             res = res + line.getLength();
         }
         return res;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        PolyLine polyLine = (PolyLine) o;
+        List<Point> pointsThis = this.getPoints();
+        List<Point> pointsPolyLine = polyLine.getPoints();
+        if (pointsThis.size() != pointsPolyLine.size()) return false;
+        for (int i = 0; i < pointsThis.size(); i++) {
+            if (!pointsThis.get(i).equals(pointsPolyLine.get(i))) return false;
+        }
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(points);
     }
 }
