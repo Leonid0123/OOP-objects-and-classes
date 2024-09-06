6af9d35724a4027b3633f44ac7223a65a902264c
lpoluyahtov <len4ik-0123@yandex.ru>
2024-09-06 17:40:12 +0300

    Практика ООП. Исключения #2. Необрабатываемые исключения

--- src/main/java/Sum.java
+++ src/main/java/Sum.java
@@ -1,17 +1,10 @@
 public class Sum {
-    private static double parseDouble(String str) {
-        try {
-            return Double.parseDouble(str);
-        } catch (NumberFormatException exception) {
-            System.out.println("Не удалось преобразовать в тип double строку " + str);
-            return 0;
-        }
-    }
-
     public static double calculate(String[] strings) {
         double res = 0;
         for (String str : strings) {
-            res += parseDouble(str);
+            if (str.matches("[-+]?\\d*\\.?\\d+"))
+                res += Double.parseDouble(str);
+            else System.out.println(str + " не является числом и не будет учитываться при суммировании.");
         }
         return res;
     }
