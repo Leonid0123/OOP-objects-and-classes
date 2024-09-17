c0f0f85bb412079360b8da80c99b1ee544486961
lpoluyahtov <len4ik-0123@yandex.ru>
2024-09-18 01:39:13 +0300

    Collections. Задача #1. Попарные перестановки

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,29 @@
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(args));
-        System.out.println(Sum.calculate(args));
+        ArrayList<Integer> list = new ArrayList<>();
+        int number = 0;
+        do {
+            System.out.println("Введите четное целое число для создания списка: ");
+            String line = new Scanner(System.in).nextLine();
+            try {
+                number = Integer.parseInt(line);
+            } catch (IllegalArgumentException e) {
+                System.out.println("Введено не целое число!");
+            }
+            if (number%2!=0)
+                System.out.println("Введено нечетное число!");
+        } while (number%2!=0);
+        for (int i = 1; i <= number; i++) {
+            list.add(i);
+        }
+        ArrayList<Integer> resList = new ArrayList<>();
+        for (int i = 0; i < number; i=i+2) {
+            resList.add(list.get(i+1));
+            resList.add(list.get(i));
+        }
+        System.out.println(resList);
     }
 }
