5581b850a80f930156c0f76a78fc1b260f64ce37
lpoluyahtov <len4ik-0123@yandex.ru>
2024-09-18 19:15:39 +0300

    Collections. Задача #2. Замена ArrayList на массив

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,29 +1,44 @@
 import java.util.ArrayList;
-import java.util.Scanner;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
         ArrayList<Integer> list = new ArrayList<>();
-        int number = 0;
-        do {
-            System.out.println("Введите четное целое число для создания списка: ");
-            String line = new Scanner(System.in).nextLine();
-            try {
-                number = Integer.parseInt(line);
-            } catch (IllegalArgumentException e) {
-                System.out.println("Введено не целое число!");
+        list.add(3);
+        list.add(6);
+        list.add(2);
+        list.add(4);
+        list.add(1);
+
+        Integer[] arr = new Integer[list.size()];
+        arr = list.toArray(arr);
+        bubbleSort(list);
+        bubbleSort(arr);
+        System.out.println(list);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static void bubbleSort(ArrayList<Integer> intList) {
+        for (int i = 0; i < intList.size() - 1; i++) {
+            for (int j = 0; j < intList.size() - i - 1; j++) {
+                if (intList.get(j) > intList.get(j + 1)) {
+                    int temp = intList.get(j);
+                    intList.set(j, intList.get(j + 1));
+                    intList.set(j + 1, temp);
+                }
             }
-            if (number%2!=0)
-                System.out.println("Введено нечетное число!");
-        } while (number%2!=0);
-        for (int i = 1; i <= number; i++) {
-            list.add(i);
         }
-        ArrayList<Integer> resList = new ArrayList<>();
-        for (int i = 0; i < number; i=i+2) {
-            resList.add(list.get(i+1));
-            resList.add(list.get(i));
+    }
+
+    public static void bubbleSort(Integer[] intArray) {
+        for (int i = 0; i < intArray.length - 1; i++) {
+            for (int j = 0; j < intArray.length - i - 1; j++) {
+                if (intArray[j] > intArray[j + 1]) {
+                    int temp = intArray[j];
+                    intArray[j] = intArray[j+1];
+                    intArray[j+1] = temp;
+                }
+            }
         }
-        System.out.println(resList);
     }
 }
