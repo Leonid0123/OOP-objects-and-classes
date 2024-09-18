cefd5f69a1a7e1026e4a81f972c5619f11691fb0
lpoluyahtov <len4ik-0123@yandex.ru>
2024-09-18 19:56:03 +0300

    Collections. Задача #3. Замена массива на ArrayList

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,41 +4,35 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         ArrayList<Integer> list = new ArrayList<>();
-        list.add(3);
-        list.add(6);
-        list.add(2);
-        list.add(4);
         list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
 
         Integer[] arr = new Integer[list.size()];
         arr = list.toArray(arr);
-        bubbleSort(list);
-        bubbleSort(arr);
+        reverse(list);
+        reverse(arr);
         System.out.println(list);
         System.out.println(Arrays.toString(arr));
     }
 
-    public static void bubbleSort(ArrayList<Integer> intList) {
-        for (int i = 0; i < intList.size() - 1; i++) {
-            for (int j = 0; j < intList.size() - i - 1; j++) {
-                if (intList.get(j) > intList.get(j + 1)) {
-                    int temp = intList.get(j);
-                    intList.set(j, intList.get(j + 1));
-                    intList.set(j + 1, temp);
-                }
-            }
+    public static void reverse(Integer[] intArray) {
+        int n = intArray.length - 1;
+        for (int i = 0; i < intArray.length / 2; i++) {
+            int temp = intArray[i];
+            intArray[i] = intArray[n - i];
+            intArray[n - i] = temp;
         }
     }
 
-    public static void bubbleSort(Integer[] intArray) {
-        for (int i = 0; i < intArray.length - 1; i++) {
-            for (int j = 0; j < intArray.length - i - 1; j++) {
-                if (intArray[j] > intArray[j + 1]) {
-                    int temp = intArray[j];
-                    intArray[j] = intArray[j+1];
-                    intArray[j+1] = temp;
-                }
-            }
+    public static void reverse(ArrayList<Integer> list) {
+        int n = list.size() - 1;
+        for (int i = 0; i < list.size() / 2; i++) {
+            int temp = list.get(i);
+            list.set(i,list.get(n-i));
+            list.set(n-i, temp);
         }
     }
 }
