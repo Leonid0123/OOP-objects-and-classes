54beb4d67c0aadfe28dd443c8a69582eceead2cb
lpoluyahtov <len4ik-0123@yandex.ru>
2024-09-23 12:49:27 +0300

    Stream API. Задача #1. Преобразование потока строк в строку

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,38 +1,16 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayList<Integer> list = new ArrayList<>();
-        list.add(1);
-        list.add(2);
-        list.add(3);
-        list.add(4);
-        list.add(5);
-
-        Integer[] arr = new Integer[list.size()];
-        arr = list.toArray(arr);
-        reverse(list);
-        reverse(arr);
-        System.out.println(list);
-        System.out.println(Arrays.toString(arr));
+        ArrayList<String> strings = new ArrayList<>(Arrays.asList("один", "два", "три", "четыре"));
+        Stream<String> stringStream = strings.stream();
+        System.out.println(getStringFromStream(stringStream));
     }
 
-    public static void reverse(Integer[] intArray) {
-        int n = intArray.length - 1;
-        for (int i = 0; i < intArray.length / 2; i++) {
-            int temp = intArray[i];
-            intArray[i] = intArray[n - i];
-            intArray[n - i] = temp;
-        }
-    }
-
-    public static void reverse(ArrayList<Integer> list) {
-        int n = list.size() - 1;
-        for (int i = 0; i < list.size() / 2; i++) {
-            int temp = list.get(i);
-            list.set(i,list.get(n-i));
-            list.set(n-i, temp);
-        }
+    public static String getStringFromStream(Stream<String> stringStream) {
+        return stringStream.map(n -> n + " ").collect(Collectors.joining());
     }
 }
